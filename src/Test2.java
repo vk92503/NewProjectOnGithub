d6155d3b383a3ce38dbb0c95344a02f2f39b85fc
V <V@V-VAIO>
2020-01-15 15:39:20 +0530

    Commiting to Test2 to BranchX...

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Y") {
+			System.out.println("Eat something...");
+		}
+		
+		else {
+			System.out.println("Do work...");
+		}
+	}
+
+}
