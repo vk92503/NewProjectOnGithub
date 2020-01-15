84cfa084d517d63c00544b437b518b8389394c2a
V <V@V-VAIO>
2020-01-15 13:48:19 +0530

    First commit NewProjectOnGithub

---
+++ src/Test1.java
@@ -0,0 +1,19 @@
+
+public class Test1 {
+	
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Y") {
+			
+			System.out.println("Eat something..");
+		}
+		
+		else {
+			
+			System.out.println("Do work..");
+		}
+	}
+
+}
